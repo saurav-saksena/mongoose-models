3aa2b31306f434134739c954524d3fb9df4588e1
saurav-saksena <sauravkumar4363@gmail.com>
2024-04-09 11:03:30 +0530

    created model for Todo-Application with help of mongoose and express js

---
+++ index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import "dotenv/config";
+const app = express();
+const port = process.env.PORT;
+
+const data = [
+  { name: "saurav", id: 1 },
+  { name: "kumar", id: 2 },
+  { name: "amit", id: 3 },
+  { name: "aman", id: 4 },
+];
+app.get("/", (req, res) => {
+  res.send("<h1>App is running successfully by Saurav Saksena ! <h1>");
+});
+
+app.get("/about", (req, res) => {
+  res.send("<h2>Author is Kumar saruav saksena <h2>");
+});
+
+app.get("/data", (req, res) => {
+  res.json(data);
+});
+
+app.listen(port, () => {
+  console.log("app is running at http://localhost:" + port);
+});
