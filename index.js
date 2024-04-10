172682abd0541a9cac6fe7c4b49c5a7e7dbcabc5
saurav-saksena <sauravkumar4363@gmail.com>
2024-04-10 09:00:16 +0530

    created Schema & model for ecommerce application

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ import "dotenv/config";
 const app = express();
 const port = process.env.PORT;
 
-const data = [
-  { name: "saurav", id: 1 },
-  { name: "kumar", id: 2 },
-  { name: "amit", id: 3 },
-  { name: "aman", id: 4 },
-];
+const data = {
+  user: [
+    { name: "saurav", id: 1 },
+    { name: "kumar", id: 2 },
+    { name: "amit", id: 3 },
+    { name: "aman", id: 4 },
+  ],
+  freshData: true,
+};
 app.get("/", (req, res) => {
   res.send("<h1>App is running successfully by Saurav Saksena ! <h1>");
 });
